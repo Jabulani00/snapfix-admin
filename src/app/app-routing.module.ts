817140639a850853bf05179d166fde21a61ff1ea
Jabulani00 <jabulanimosesgwala@gmail.com>
2023-02-27 11:22:43 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'reset',
+    loadChildren: () => import('./reset/reset.module').then( m => m.ResetPageModule)
+  },  {
+    path: 'dashboard-admin',
+    loadChildren: () => import('./dashboard-admin/dashboard-admin.module').then( m => m.DashboardAdminPageModule)
+  },
+  {
+    path: 'pending',
+    loadChildren: () => import('./pending/pending.module').then( m => m.PendingPageModule)
+  },
+  {
+    path: 'reported',
+    loadChildren: () => import('./reported/reported.module').then( m => m.ReportedPageModule)
+  },
+  {
+    path: 'completed',
+    loadChildren: () => import('./completed/completed.module').then( m => m.CompletedPageModule)
+  },
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/reported/reported.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reported',
+  templateUrl: './reported.page.html',
+  styleUrls: ['./reported.page.scss'],
+})
+export class ReportedPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
